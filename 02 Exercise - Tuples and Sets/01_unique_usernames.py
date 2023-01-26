ef6e52dcefe4990c81a496323288c840e1f3caef
pbekriev <petar.bekriev@gmail.com>
2023-01-26 08:19:56 +0200

    Update 01_unique_usernames.py

--- 02 Exercise - Tuples and Sets/01_unique_usernames.py
+++ 02 Exercise - Tuples and Sets/01_unique_usernames.py
@@ -1,7 +1,9 @@
-names_count = int(input())
-names = set()
+# names_count = int(input())
+# names = set()
+#
+# for _ in range (names_count):
+#     names.add(input())
+#
+# print(*names, sep="\n")
 
-for _ in range (names_count):
-    names.add(input())
-
-print(*names, sep="\n")
+print(*{input() for _ in range(int(input()))}, sep="\n")
