004c0df263ded9a791bc8fe7617738c83bc40753
pbekriev <petar.bekriev@gmail.com>
2023-01-26 13:28:48 +0200

    Update 06_battle_of_names.py

---
+++ 02 Exercise - Tuples and Sets/06_battle_of_names.py
@@ -0,0 +1,17 @@
+odd_set = set()
+even_set = set()
+
+for row in range(1, int(input()) + 1):
+    ascii_sum_of_name = sum(ord(l) for l in input()) // row
+
+    if ascii_sum_of_name % 2 == 0:
+        even_set.add(ascii_sum_of_name)
+    else:
+        odd_set.add(ascii_sum_of_name)
+
+if sum(odd_set) == sum(even_set):
+    print(*odd_set.union(even_set), sep=", ")
+elif sum(odd_set) > sum(even_set):
+    print(*odd_set.difference(even_set), sep=", ")
+else:
+    print(*odd_set.symmetric_difference(even_set), sep=", ")
