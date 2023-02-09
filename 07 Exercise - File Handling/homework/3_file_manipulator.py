d430d9521164d9f78cd131ac62b1ba467e9f398a
pbekriev <petar.bekriev@gmail.com>
2023-02-09 23:16:35 +0200

    homework

---
+++ 07 Exercise - File Handling/homework/3_file_manipulator.py
@@ -0,0 +1,34 @@
+import os
+
+while True:
+    command, *info = input().split("-")
+
+    if command == "Create":
+        file = open(f"files/{info[0]}", "w")
+        file.close()
+
+    elif command == "Add":
+        with open(f"files/{info[0]}", "a") as file:
+            file.write(f"{info[1]}\n")
+
+    elif command == "Replace":
+        try:
+            with open(f"files/{info[0]}", "r") as file:
+                text = file.readlines()
+
+            for i in range(len(text)):
+                text[i] = text[i].replace(info[1], info[2])
+
+            with open(f"files/{info[0]}", "w") as file:
+                file.write("".join(text))
+        except FileNotFoundError:
+            print(f"An error occurred")
+
+    elif command == "Delete":
+        try:
+            os.remove(f"files/{info[0]}")
+        except FileNotFoundError:
+            print("An error occurred")
+
+    elif command == "End":
+        break
---
+++ 07 Exercise - File Handling/homework/4_directory_traversal.py
@@ -0,0 +1,35 @@
+import os
+
+
+def save_extensions(dir_name, first_level=False):
+    for filename in os.listdir(dir_name):
+        file = os.path.join(dir_name, filename)  # dir_name + / + filename
+
+        if os.path.isfile(file):
+            extension = filename.split(".")[-1]
+
+            if extension not in extensions:
+                extensions[extension] = []
+
+            extensions[extension].append(filename)
+
+        elif os.path.isdir(file) and not first_level:  # first_level == False
+            save_extensions(file, first_level=True)
+
+
+directory = input()
+extensions = {}  # ex. {.py: [hello_world.py, z_file.py]}
+result = []
+
+save_extensions(directory)
+
+extensions = sorted(extensions.items(), key=lambda x: x[0])
+
+for extension, files in extensions:
+    result.append(f".{extension}")
+
+    for file in sorted(files):
+        result.append(f"- - -{file}")
+
+with open(f"files/report.txt", "w") as file:
+    file.write("\n".join(result))
